2780b0af87af5f35aa36b4e36ad9f6c02febf9b7
Shodai Yokoyama <quantumcars@gmail.com>
2015-09-01 22:45:50 +0900

    project.clj: Add configs for uberjar

--- project.clj
+++ project.clj
@@ -7,6 +7,8 @@
                  [ring/ring-defaults "0.1.2"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler hello-world.handler/app}
+  :main hello-world.handler
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+                        [ring-mock "0.1.5"]]}
+   :uberjar {:aot :all}})
